6566fc01abb5fbad87ab44061b2fd33cfa9539d9
GingShan <105120159+GingShan@users.noreply.github.com>
2023-08-17 11:24:01 +0800

    Add files via upload

---
+++ Demo02/js/nav.js
@@ -0,0 +1,21 @@
+$(function () {
+    let windowWidth = window.innerWidth;
+    // if (windowWidth <= 991) {
+    //     $(".MenuIcon").click(() => {
+    //         $(".MenuIcon").toggleClass("dispear");
+    //         $(".navbar").toggleClass("Sticky", 800);
+    //     });
+    // } else {
+    //     $(".MenuIcon").click(() => {
+    //         $(".MenuIcon").addClass("dispear");
+    //         $(".navbar").addClass("Sticky", 800);
+    //     });
+    //     $(".navbar").mouseleave(() => {
+    //         $(".MenuIcon").removeClass("dispear");
+    //         $(".navbar").removeClass("Sticky", 800);
+    //     });
+    // }
+    $(".MenuIcon").click(() => {
+        $(".navbar").toggleClass("appear");
+    });
+})
